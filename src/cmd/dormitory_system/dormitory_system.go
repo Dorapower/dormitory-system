3d6e1ed13c17a1c4c8a1b9520b67b18af8a1d9fb
CircleFF <gtliutongfa@foxmail.com>
2022-11-12 00:26:38 +0800

    Add connection to mysql

--- src/cmd/dormitory_system/dormitory_system.go
+++ src/cmd/dormitory_system/dormitory_system.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"dormitory-system/src/database"
 	"dormitory-system/src/router"
 	"log"
 )
 
 func main() {
+	database.ConnectMysql()
 	err := router.InitRouter().Run(":8090")
 	if err != nil {
 		log.Fatalln("Server Error: ", err)
