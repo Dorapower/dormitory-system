ef5ea686b15ceff1623ca884924ea288df5024ed
CircleFF <gtliutongfa@foxmail.com>
2022-11-14 22:11:50 +0800

    add two routers for adding data

--- src/router/router.go
+++ src/router/router.go
@@ -1,6 +1,7 @@
 package router
 
 import (
+	"dormitory-system/src/addData"
 	"dormitory-system/src/auth"
 	"github.com/gin-gonic/gin"
 )
@@ -13,6 +14,10 @@ func InitRouter() *gin.Engine {
 	api := router.Group("/api")
 	api.POST("/auth/login", auth.LoginHandler)
 	api.POST("/auth/refresh_token", auth.RefreshHandler)
+
+	add := router.Group("/add")
+	add.POST("/auth", addData.AddAuth)
+	add.POST("/user", addData.AddUser)
 
 	return router
 }
