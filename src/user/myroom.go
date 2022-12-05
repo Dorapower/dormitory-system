b8dcdef3954a7835bfa9eee5baad595f987de10e
Dorapower <thatisluo@outlook.com>
2022-12-05 20:57:47 +0800

    user api is supposedly bug-free

--- src/user/myroom.go
+++ src/user/myroom.go
@@ -9,15 +9,23 @@ import (
 func MyRoomHandler(ctx *gin.Context) {
 	uid := ctx.Keys["uid"].(int)
 	room, roommate := model.GetMyRoomByUid(uid)
-	ctx.JSON(http.StatusOK,
-		gin.H{
-			"code": 200,
-			"msg":  "success",
-			"data": gin.H{
-				"roomName": room,
-				"member": gin.H{
-					"rows": roommate,
-				},
-			},
+	if room == "" {
+		ctx.JSON(http.StatusOK, gin.H{
+			"code":    1,
+			"message": "you don't have a room yet",
+			"data":    gin.H{},
 		})
+	} else {
+		ctx.JSON(http.StatusOK,
+			gin.H{
+				"code":    200,
+				"message": "success",
+				"data": gin.H{
+					"roomName": room,
+					"member": gin.H{
+						"rows": roommate,
+					},
+				},
+			})
+	}
 }
--- src/user/passwd.go
+++ src/user/passwd.go
@@ -7,8 +7,8 @@ import (
 )
 
 type PasswordRequest struct {
-	OldPassword string `json:"old_password" binding:"required"`
-	NewPassword string `json:"new_password" binding:"required"`
+	OldPassword string `json:"oldPasswd" binding:"required"`
+	NewPassword string `json:"newPasswd" binding:"required"`
 }
 
 func PasswdHandler(ctx *gin.Context) {
@@ -19,21 +19,18 @@ func PasswdHandler(ctx *gin.Context) {
 		ctx.JSON(400, gin.H{
 			"error_code": 1,
 			"message":    "bad request",
-			"data":       gin.H{},
 		})
 	}
 	uid := ctx.Keys["uid"].(int)
 	if model.ChangePwd(uid, passwordRequest.OldPassword, passwordRequest.NewPassword) == false {
 		ctx.JSON(500, gin.H{
-			"error_code": 1,
-			"message":    "old password is wrong",
-			"data":       gin.H{},
+			"code":    2,
+			"message": "old password is wrong",
 		})
 		return
 	}
 	ctx.JSON(200, gin.H{
-		"error_code": 0,
-		"message":    "change password success",
-		"data":       gin.H{},
+		"code":    200,
+		"message": "change password success",
 	})
 }
