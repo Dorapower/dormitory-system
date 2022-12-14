4a3d6bb3ac110d2d72c82186b528a04517f11217
MyCircles <gtliutongfa@foxmail.com>
2022-12-14 20:35:20 +0800

    Delete group not need "TeamId" now

--- src/team/deleteTeam.go
+++ src/team/deleteTeam.go
@@ -5,24 +5,9 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type DeleteTeamRequest struct {
-	TeamId int `form:"team_id" json:"team_id" binding:"required"`
-}
-
 func DeleteTeamHandler(ctx *gin.Context) {
-	// delete teams based on requests
-	// uid := ctx.Keys["uid"].(int)
-	var deleteTeamRequest DeleteTeamRequest
-	err := ctx.ShouldBind(&deleteTeamRequest)
-	if err != nil {
-		ctx.JSON(400, gin.H{
-			"code":    1,
-			"message": "bad request",
-			"data":    gin.H{},
-		})
-		return
-	}
-	success := model.DelGroup(deleteTeamRequest.TeamId)
+	uid := ctx.Keys["uid"].(int)
+	success := model.DelGroup(uid)
 	if success {
 		ctx.JSON(200, gin.H{
 			"code":    200,
